7fab60a0e0cf58aee68dbd54542546a06820e604
SteveStar <71157526+SteveStar@users.noreply.github.com>
2024-11-29 19:02:08 -0500

    Added Object to JSON

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+//OBJECTS
 student = {
     name: "Steve",
     age: 23,
     enrolled: true,
     courses: ["Web Dev", " Javascript", " Algorithms", " Databases"],
     studentInfo: function() {
-        console.log(`\nStudent name: ${this.name}\nStudent age: ${this.age}\nCurrent enrolled classes: ${this.courses}\nis enrolled?: ${this.enrolled}`);
+        console.log(`\nStudent name: ${this.name}\nStudent age: ${this.age}\nCurrent enrolled classes: ${this.courses}\nis enrolled?: ${this.enrolled}\n`);
     }
 }
 
 console.log(`My name is ${student.name} and I am ${student.age} years old`);
 
 student.studentInfo();
+
+//OBJECT -> JSON
+
+let studentString = '{"name": "Steve", "age": 23, "enrolled": true, "courses": ["Web Dev", "Javascript", "Algorithms", "Databases"]}\n';
+
+console.log(studentString);
+
+
